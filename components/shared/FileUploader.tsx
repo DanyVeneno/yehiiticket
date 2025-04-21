175b70ce899ee9164288cfbdfd6bc91dc7639f81
DanyVeneno <danieledjv@gmail.com>
2025-04-20 20:32:50 -0600

    corregimos una condicion

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -19,10 +19,13 @@ export function FileUploader({
   onFieldChange,
   setFiles,
 }: FileUploaderProps) {
-  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
-    setFiles(acceptedFiles);
-    onFieldChange(convertFileToUrl(acceptedFiles[0]));
-  }, []);
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      setFiles(acceptedFiles);
+      onFieldChange(convertFileToUrl(acceptedFiles[0]));
+    },
+    [setFiles, onFieldChange]
+  );
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
